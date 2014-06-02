c1bc88cd009074cee0a62e59556d55f2e5c44531
Ivarz <ivarssila@inbox.lv>
2014-06-02 23:31:31 +0300

    vcf_apstrade_nonjemtas_liekas_funkcijas

--- calling/process_vcf.py
+++ calling/process_vcf.py
@@ -10,6 +10,7 @@ saraksts=glob.glob("*.vcf")
 names_pop=[x.replace(".samtools.vcf","").replace(".gatk_ug.vcf","").replace(".varscan.vcf","") for x in saraksts]
 names_uniq=set(names_pop)
 for name in names_uniq:
+	print name
 	sam=read_vcf(name+".samtools.vcf")
 	gatk=read_vcf(name+".gatk_ug.vcf")
 	varscan=read_vcf(name+".varscan.vcf")
@@ -65,77 +66,6 @@ def write_vcf(name1,name2,good_vcf):
     for i in range(0, len(good_vcf)):
         writer.write_record(good_vcf[i])
     writer.close()
-
-def het_filter(vcf_raw,het_thresh):
-    
-    vcf_cont=[]    
-    for rec in vcf_raw:
-        i=0
-        j=0
-        for sample in rec.samples:
-            if sample.data[0]!=None:
-                if sample.data[0]=="1/1" or sample.data[0]=="0/1" or sample.data[0]=="0/0":
-                    het=float(sample.data[1][0])/(sample.data[1][0]+sample.data[1][1])
-                    if het<0.5:
-                        het=1-het
-                elif sample.data[0]=="2/2" or sample.data[0]=="0/2":
-                    het=float(sample.data[1][0])/(sample.data[1][0]+sample.data[1][2])
-                    if het<0.5:
-                        het=1-het
-                elif sample.data[0]=="1/2":
-                    het=float(sample.data[1][1])/(sample.data[1][2]+sample.data[1][1])
-                    if het<0.5:
-                        het=1-het
-                elif sample.data[0]=="1/3":
-                    het=float(sample.data[1][1])/(sample.data[1][3]+sample.data[1][1])
-                    if het<0.5:
-                        het=1-het
-                elif sample.data[0]=="2/3":
-                    het=float(sample.data[1][2])/(sample.data[1][3]+sample.data[1][2])
-                    if het<0.5:
-                        het=1-het
-                elif sample.data[0]=="3/3" or sample.data[0]=="0/3":
-                    het=float(sample.data[1][0])/(sample.data[1][0]+sample.data[1][3])
-                    if het<0.5:
-                        het=1-het
-                if het<het_thresh:
-                    j+=1
-            else:
-                j+=1
-
-        if i==0 and j==0:
-            vcf_cont.append(rec)
-    return vcf_cont
-
-
-#visas pozicijas ar klaster snpiem. Ja kadam paraugam snp ir klasteri, tad abas pozicijas tiek ieliktas saraksta
-
-def clst_filter(vcf_cont,clst_thresh): 
-    pos=dict()
-    for sample in vcf_cont[0].samples:
-        pos[sample.sample]=[]	#dictionary inicializacija
-    for rec in vcf_cont:
-        for sample in rec.samples:
-            if sample.data[0]!=None:            
-                if sample.data[0]!="0/0":
-                    pos[sample.sample].append(rec.POS)
-    tmp=[]
-    for sample in pos.keys():
-        if len(pos[sample])>0:
-            if np.abs(pos[sample][0]-pos[sample][1])<=clst_thresh:	#parbauda pirmo poziciju
-               tmp.append(pos[sample][0])
-            for i in range(1,len(pos[sample])-1):
-                if np.abs(pos[sample][i-1]-pos[sample][i])<=clst_thresh or np.abs(pos[sample][i]-pos[sample][i+1])<=clst_thresh:
-                    tmp.append(pos[sample][i])
-            if np.abs(pos[sample][-1]-pos[sample][-2])<=clst_thresh:	#parbauda pedejo poziciju
-                tmp.append(pos[sample][-1])
-    bad_pos=sorted(set(tmp))
-    good_vcf=[]
-    i=0
-    for rec in vcf_cont:
-        if not rec.POS in bad_pos:
-            good_vcf.append(rec)
-    return good_vcf
 
 def cover_filter(vcf_cont,cov_thresh):
     good_vcf=[]
