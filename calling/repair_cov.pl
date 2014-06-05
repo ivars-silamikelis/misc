8a47498b191c8ac774f75159b696a51424b3da4c
Ivarz <ivarssila@inbox.lv>
2014-06-05 23:10:44 +0300

    repair_coverage_file

---
+++ calling/repair_cov.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl;
+use strict;
+use warnings;
+my $fname=shift;
+open FH ,"<", $fname or die "No such file\n";
+$name=~s/\.cov$//;
+open OUT,">", "repaired/$fname.repaired.cov";
+while (<FH>){
+	chomp($_);
+	my @fields=split("\t",$_);
+	unless ($fields[2]){
+		$fields[2]=$fields[1];	
+	}
+	my $line=join("\t", @fields);
+	print OUT $line,"\n";
+}
+close FH;
+close OUT;
+
+
